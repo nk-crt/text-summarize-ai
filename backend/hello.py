62d49d73d8160b558ad969450f315a009b98636a
Nakul <nkcrt16@gmail.com>
2026-06-17 13:59:41 +0530

    Updated backend

--- backend/hello.py
+++ backend/hello.py
@@ -2,9 +2,19 @@ from groq import Groq
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
-
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
 app=FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 load_dotenv()
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY")
@@ -13,22 +23,24 @@ client = Groq(
 class type(BaseModel):
     text:str
 def ai(query):
-    response=client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {
-                "role":"system",
-                "content":"summarize the whole text in four bullet point"
+    try:
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "summarize the whole text in four bullet point"
+                },
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ]
+        )
+        return response.choices[0].message.content
 
-            },
-            {
-                "role":"user",
-                "content":query
-            }
-        ]
-
-    )
-    return response.choices[0].message.content
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 @app.get("/")
 def home():
